07915d99cd123433e287621457ad1cf531f3b351
Phat <phatdoan.1014@gmail.com>
2024-09-24 16:32:20 +0700

    Associations

--- helpers/joi_schema.js
+++ helpers/joi_schema.js
@@ -1,4 +1,5 @@
 import joi from 'joi';
 
+// Validate email and password
 export const email = joi.string().pattern(new RegExp('gmail.com$')).required();
 export const password = joi.string().min(6).required();
